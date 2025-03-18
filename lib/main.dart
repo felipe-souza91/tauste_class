893ae2791c342b616dfbe72d65e3fe790d70b8bf
felipe-souza91 <felipe_souza91@live.com>
2025-03-17 21:50:40 -0300

    Criado menu Sandwich

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,49 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+  @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    super.setState(fn);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Tauste Supermercados',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              Center(
+                child: Text('Departamentos',
+                style:TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                )),
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Marilia'),
+        ),
+        body: Center(
+          child: Text('Bem- vindo ao Tauste')
+        )
+      )
+    );
   }
 }
